7084d6e04ab719eec4abee7c0ede9451c4ec28ee
Alexander8879 <ignacio.as653@gmail.com>
2026-05-23 18:51:53 -0400

    Agregar Dockerfile y pipeline

--- FarnaciaDevOps/obj/Debug/net10.0/FarnaciaDevOps.AssemblyInfo.cs
+++ FarnaciaDevOps/obj/Debug/net10.0/FarnaciaDevOps.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FarnaciaDevOps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c8bc5ec62510a41c610f0ecef479f107b44dd70")]
 [assembly: System.Reflection.AssemblyProductAttribute("FarnaciaDevOps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FarnaciaDevOps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
